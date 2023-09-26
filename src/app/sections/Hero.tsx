687e5c0220e3c31e749d01c1ca74ddbbfa9a312d
Naman <naman@flexpool.io>
2023-09-25 21:37:34 -0700

    Fix wording

--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -15,8 +15,8 @@ export const Hero: React.FC<HeroProps> = ({ }) => {
                   <div className="flex flex-row items-start w-full">
                         <div className='mr-8'>
                               <p className="h-10 text-2xl md:text-3xl font-bold">Hi, I&apos;m Naman.</p>
-                              <p className="text-lg md:text-xl">I love making ideas come to life with code. I&apos;m a huge <Link href={"https://go.dev/"}>Golang</Link> and <Link href={"https://www.typescriptlang.org/"}>TypeScript</Link> fan.</p>
-                              <p className="text-lg md:text-xl mt-8">I&apos;m currently interested in blockchain programming and system design. I also enjoy reading in my free time. Based in San Diego, CA.</p>
+                              <p className="text-lg md:text-xl">I love making ideas come to life with code. I&apos;m a huge fan of <Link href={"https://go.dev/"}>Golang</Link> and <Link href={"https://www.typescriptlang.org/"}>TypeScript</Link>.</p>
+                              <p className="text-lg md:text-xl mt-8">I&apos;m currently interested in blockchain programming and system design. I also enjoy reading in my free time. <br/> Based in San Diego, CA.</p>
                               <Biolinks />
                         </div>
                         <div className={"hidden md:flex"}>
