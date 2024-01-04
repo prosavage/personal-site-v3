a5f8bae3a5736b96b62e0c694eed7bea94ec6aab
Naman <naman0311@gmail.com>
2024-01-03 22:00:48 -0600

    Fix level coloring on last year view and use official API for last year instead of stitching it ourselves

--- src/app/api/cron/contribution-stats/route.ts
+++ src/app/api/cron/contribution-stats/route.ts
@@ -14,8 +14,10 @@ export async function GET() {
     const data: ContributionData = await result.json();
     const sortedContributions: Record<string, Contribution[]> = {};
 
-  
-    
+    const totals = data.total;
+
+
+
     sortedContributions["last-year"] = [];
     for (let i = 0; i < data.contributions.length; i++) {
         const contribution = data.contributions[i];
@@ -27,40 +29,23 @@ export async function GET() {
         sortedContributions[year].push(contribution);
     }
 
-    const todaysDate = new Date();
-    // 0 pad month and day
-    const month = (todaysDate.getMonth() + 1).toString().padStart(2, "0");
-    const day = todaysDate.getDate().toString().padStart(2, "0");
-    const todaysDateString = `${todaysDate.getFullYear()}-${month}-${day}`;
+    const resultLastYear = await fetch(
+        "https://github-contributions-api.jogruber.de/v4/prosavage?y=last",
+        {
+            cache: "no-store",
+        },
+    );
 
-    // Find the days before today's date and add them to the last year array
-    const todaysYear = todaysDate.getFullYear().toString();
-    const thisYearsDays = sortedContributions[todaysYear];
-  
-    for (const day of thisYearsDays) {
-        sortedContributions["last-year"].push(day);
-        if (day.date === todaysDateString) {
-            break;
-        }
-    }
+    const lastYearData: ContributionData = await resultLastYear.json();
+    sortedContributions["last-year"] = lastYearData.contributions;
 
-    // So now we have all the days until today in the last year array.
-    // That means the rest of the days are from the year before.
-    // So we have like Jan 1, Jan 2, Jan 3
-    // So we can append the end of the previous year's array elements.
-    const daysWeNeedFromPreviousYear = 365 - sortedContributions["last-year"].length;
-    const previousYear = (todaysDate.getFullYear() - 1).toString();
-    const previousYearDays = sortedContributions[previousYear];
-    const slicedDays = previousYearDays.slice(previousYearDays.length - daysWeNeedFromPreviousYear);
-    sortedContributions["last-year"] = slicedDays.concat(sortedContributions["last-year"]);
-
-    data.total["last-year"] = sortedContributions["last-year"].reduce((acc, curr) => acc + curr.count, 0);
-
+    totals["last-year"] = lastYearData.total["lastYear"];
 
     await kv.set("contributions", JSON.stringify(sortedContributions));
-    await kv.set("totals", JSON.stringify(data.total));
+    await kv.set("totals", JSON.stringify(totals));
 
-    console.log("Updated contributions cache", new Date().toTimeString());
+    const timestamp = new Date().toTimeString();
+    console.log("Updated contributions cache", timestamp);
 
-    return Response.json({ sortedContributions });
+    return Response.json({ sortedContributions, totals, timestamp });
 }
