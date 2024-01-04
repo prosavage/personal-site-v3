4f88ba28daf7299d159028ebf3c59f199d1e1e2e
Naman <naman0311@gmail.com>
2024-01-03 22:17:23 -0600

    Add new speed insights and remove vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -25,7 +25,7 @@ export default function RootLayout({
           {children}
         </main>
       </body>
-      <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
