1cc4df737735746d9ef3173be444438161112360
Naman <naman0311@gmail.com>
2024-01-03 22:20:16 -0600

    Add analytics back

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,30 +2,32 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const poppins = Poppins({
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  subsets: ["latin"]
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    subsets: ["latin"]
 });
 
 export const metadata: Metadata = {
-  title: "Naman Gupta: Home",
-  description: "Naman Gupta's personal website.",
+    title: "Naman Gupta: Home",
+    description: "Naman Gupta's personal website.",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={poppins.className}>
-        <main className="flex min-h-screen flex-col justify-start p-2 sm:p-12 md:p-24 items-center">
-          {children}
-        </main>
-      </body>
-      <SpeedInsights />
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={poppins.className}>
+                <main className="flex min-h-screen flex-col justify-start p-2 sm:p-12 md:p-24 items-center">
+                    {children}
+                </main>
+            </body>
+            <SpeedInsights />
+            <Analytics />
+        </html>
+    );
 }
