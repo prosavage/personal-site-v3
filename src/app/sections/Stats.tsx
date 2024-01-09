0b1d33d3c595d5e83a11c3034831b04a905c1951
Naman <naman0311@gmail.com>
2024-01-08 20:42:00 -0800

    Just build the page data every hour instead of storing in kv.

--- src/app/sections/Stats.tsx
+++ src/app/sections/Stats.tsx
@@ -1,8 +1,7 @@
 import { Separator } from "@/components/Separator";
 import { ContributionChart } from "@/components/root/stats/ContributionChart";
-import { kv } from "@vercel/kv";
 import { cache } from "react";
-import { Contribution } from "../util/Contribution";
+import { Contribution, ContributionData } from "../util/Contribution";
 
 interface StatsProps {
 }
@@ -10,10 +9,44 @@ interface StatsProps {
 // Using cache to only refresh contributions once per hour.
 // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#fetching-data-on-the-server-with-third-party-libraries
 export const getContributionData = cache(async () => {
-    const contributions = (await kv.get<Record<string, Contribution[]>>("contributions")) ?? {};
-    const totals = (await kv.get<Record<string, number>>("totals")) ?? {};
+    const result = await fetch(
+        "https://github-contributions-api.jogruber.de/v4/prosavage",
+        {
+            cache: "no-store",
+        },
+    );
 
-    return [contributions, totals] as const;
+    const data: ContributionData = await result.json();
+    const sortedContributions: Record<string, Contribution[]> = {};
+
+    const totals = data.total;
+
+
+
+    sortedContributions["last-year"] = [];
+    for (let i = 0; i < data.contributions.length; i++) {
+        const contribution = data.contributions[i];
+        const year = contribution.date.split("-")[0];
+        if (!sortedContributions[year]) {
+            sortedContributions[year] = [];
+        }
+
+        sortedContributions[year].push(contribution);
+    }
+
+    const resultLastYear = await fetch(
+        "https://github-contributions-api.jogruber.de/v4/prosavage?y=last",
+        {
+            cache: "no-store",
+        },
+    );
+
+    const lastYearData: ContributionData = await resultLastYear.json();
+    sortedContributions["last-year"] = lastYearData.contributions;
+
+    totals["last-year"] = lastYearData.total["lastYear"];
+
+    return [sortedContributions, totals] as const;
 });
 
 export const Stats: React.FC<StatsProps> = async ({ }) => {
