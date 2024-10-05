7184cc282bcbe63e9b1b81a61363ec342751518d
Naman <naman0311@gmail.com>
2024-10-05 14:42:47 -0700

    Next.js 15

--- src/app/sections/Stats.tsx
+++ src/app/sections/Stats.tsx
@@ -9,14 +9,13 @@ interface StatsProps {
 // Using cache to only refresh contributions once per hour.
 // https://nextjs.org/docs/app/building-your-application/data-fetching/fetching-caching-and-revalidating#fetching-data-on-the-server-with-third-party-libraries
 export const getContributionData = cache(async () => {
-    const fetchOpts = { next: { revalidate: 3600 } };
     const result = await fetch(
         "https://gh-contributions-chart-data.fly.dev/contributions/prosavage",
-        fetchOpts,
+        { cache: "force-cache", next: { revalidate: 3600 } },
     );
 
     const data: ContributionData = await result.json();
-    
+
     const sortedContributions = data.contributions;
     const totals = data.totals;
     return [sortedContributions, totals] as const;
@@ -24,11 +23,11 @@ export const getContributionData = cache(async () => {
 
 export const Stats: React.FC<StatsProps> = async ({ }) => {
     const [contributions, totals] = await getContributionData();
-    
+
     return (
         <div className="mt-8">
-            <ContributionChart allContributions={contributions} totals={totals}  />
-            <Separator/>
+            <ContributionChart allContributions={contributions} totals={totals} />
+            <Separator />
         </div>
 
     );
